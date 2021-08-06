bff13d7413608bc8372efd1bfd56687f5701dbd0
gjwaa <1258046117@qq.com>
2021-08-06 18:28:13 +0800

    1
    1

--- pages/siteDetail/siteDetail.js
+++ pages/siteDetail/siteDetail.js
@@ -75,7 +75,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(this.data.nowLongitude)
     wx.setNavigationBarTitle({
       title: options.stationName
     })
